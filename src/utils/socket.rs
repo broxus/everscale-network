1a0857168d24b509a689179e812306e11ccd081b
Ivan Kalinin <i.kalinin@dexpa.io>
2021-07-10 02:26:40 +0300

    Set SO_REUSEPORT for udp socket

--- src/utils/socket.rs
+++ src/utils/socket.rs
@@ -12,22 +12,46 @@ pub fn make_udp_socket(port: u16) -> Result<Arc<UdpSocket>> {
         use std::os::unix::io::AsRawFd;
 
         let fd = udp_socket.as_raw_fd();
-        let size: libc::c_int = 1 << 20;
 
-        let payload = &size as *const libc::c_int as *const libc::c_void;
-        let res = unsafe {
-            libc::setsockopt(
+        let size: usize = 1 << 20;
+        unsafe { setsockopt(fd, libc::SOL_SOCKET, libc::SO_RCVBUF, size as libc::c_int)? };
+
+        unsafe {
+            setsockopt(
                 fd,
                 libc::SOL_SOCKET,
-                libc::SO_RCVBUF,
-                payload,
-                std::mem::size_of_val(&size) as libc::socklen_t,
-            )
+                libc::SO_REUSEPORT,
+                true as libc::c_int,
+            )?
         };
-        if res == -1 {
-            return Err(std::io::Error::last_os_error().into());
-        }
     }
 
     Ok(Arc::new(UdpSocket::from_std(udp_socket)?))
+}
+
+unsafe fn setsockopt<T>(
+    socket: libc::c_int,
+    level: libc::c_int,
+    name: libc::c_int,
+    value: T,
+) -> std::io::Result<()>
+where
+    T: Copy,
+{
+    let value = &value as *const T as *const libc::c_void;
+    cvt(libc::setsockopt(
+        socket,
+        level,
+        name,
+        value,
+        std::mem::size_of::<T>() as libc::socklen_t,
+    ))
+}
+
+fn cvt(res: libc::c_int) -> std::io::Result<()> {
+    if res == -1 {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
 }
