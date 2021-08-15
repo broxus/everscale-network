c50a7b88e52f551ef39871a4d2aff652ad58c7ed
Ivan Kalinin <i.kalinin@dexpa.io>
2021-08-15 13:16:31 +0300

    Fix packet contents

--- src/proto/packet_contents.rs
+++ src/proto/packet_contents.rs
@@ -1,6 +1,5 @@
 use std::io::Write;
 
-use bitflags::bitflags;
 use rand::RngCore;
 use smallvec::SmallVec;
 
@@ -25,76 +24,43 @@ pub struct PacketContentsView<'a> {
     pub signature: Option<&'a [u8]>,
 }
 
-bitflags! {
-struct Flags:u32
-    {
-        const FROM = 0x0001;
-        const FROM_SHORT = 0x0002;
-        const MESSAGE = 0x0004;
-        const MESSAGES = 0x0008;
-        const ADDRESS = 0x0010;
-        const PRIORITY_ADDRESS = 0x0020;
-        const SEQNO = 0x0040;
-        const CONFIRM_SEQNO = 0x0080;
-        const RECV_ADDR_LIST_VERSION = 0x0100;
-        const RECV_PRIORITY_ADDR_LIST_VERSION = 0x0200;
-        const REINIT_DATE = 0x0400;
-        const SIGNATURE = 0x0800;
-    }
-}
-
-impl Flags {
-    #[inline]
-    fn set_name<T>(&mut self, flag: Flags, to_set: &Option<T>) {
-        if to_set.is_some() {
-            self.toggle(flag)
-        }
-    }
-}
-
-const CONSTRUCTOR: u32 = 0xd142cd89;
+impl Boxed for PacketContentsView<'_> {}
 
 impl<'a> ReadFromPacket<'a> for PacketContentsView<'a> {
     fn read_from(packet: &'a [u8], offset: &mut usize) -> PacketContentsResult<Self> {
         let constructor = u32::read_from(packet, offset)?;
-        if constructor != CONSTRUCTOR {
+        if constructor != ID_PACKET_CONTENTS {
             return Err(PacketContentsError::UnknownConstructor);
         }
 
         read_bytes(packet, offset)?; // skip rand1
 
-        let flags = u32::read_from(packet, offset)?;
-        let flags = match Flags::from_bits(flags) {
-            Some(a) => a,
-            None => return Err(PacketContentsError::UnexpectedEof),
-        };
-        let from = read_optional(packet, offset, flags.contains(Flags::FROM))?;
-        let from_short = read_optional(packet, offset, flags.contains(Flags::FROM_SHORT))?;
+        let flags = Flags(u32::read_from(packet, offset)?);
 
-        let message = read_optional(packet, offset, flags.contains(Flags::MESSAGE))?;
-        let messages = read_optional(packet, offset, flags.contains(Flags::MESSAGES))?;
+        let from = read_optional(packet, offset, flags.contains(HAS_FROM))?;
+        let from_short = read_optional(packet, offset, flags.contains(HAS_FROM_SHORT))?;
 
-        let address = read_optional(packet, offset, flags.contains(Flags::ADDRESS))?;
-        read_optional::<AddressListView>(packet, offset, flags.contains(Flags::PRIORITY_ADDRESS))?; // skip `priority_address`
+        let message = read_optional(packet, offset, flags.contains(HAS_MESSAGE))?;
+        let messages = read_optional(packet, offset, flags.contains(HAS_MESSAGES))?;
 
-        let seqno = read_optional(packet, offset, flags.contains(Flags::SEQNO))?;
-        let confirm_seqno = read_optional(packet, offset, flags.contains(Flags::CONFIRM_SEQNO))?;
+        let address = read_optional(packet, offset, flags.contains(HAS_ADDRESS))?;
+        read_optional::<AddressListView>(packet, offset, flags.contains(HAS_PRIORITY_ADDRESS))?; // skip `priority_address`
 
-        let recv_addr_list_version = read_optional(
-            packet,
-            offset,
-            flags.contains(Flags::RECV_ADDR_LIST_VERSION),
-        )?;
+        let seqno = read_optional(packet, offset, flags.contains(HAS_SEQNO))?;
+        let confirm_seqno = read_optional(packet, offset, flags.contains(HAS_CONFIRM_SEQNO))?;
+
+        let recv_addr_list_version =
+            read_optional(packet, offset, flags.contains(HAS_RECV_ADDR_LIST_VERSION))?;
         let recv_priority_addr_list_version = read_optional(
             packet,
             offset,
-            flags.contains(Flags::RECV_PRIORITY_ADDR_LIST_VERSION),
+            flags.contains(HAS_RECV_PRIORITY_ADDR_LIST_VERSION),
         )?;
 
-        let reinit_date = read_optional(packet, offset, flags.contains(Flags::REINIT_DATE))?;
-        let dst_reinit_date = read_optional(packet, offset, flags.contains(Flags::REINIT_DATE))?;
+        let reinit_date = read_optional(packet, offset, flags.contains(HAS_REINIT_DATE))?;
+        let dst_reinit_date = read_optional(packet, offset, flags.contains(HAS_REINIT_DATE))?;
 
-        let signature = read_optional(packet, offset, flags.contains(Flags::SIGNATURE))?;
+        let signature = read_optional(packet, offset, flags.contains(HAS_SIGNATURE))?;
 
         read_bytes(packet, offset)?; // skip rand2
 
@@ -117,49 +83,106 @@ impl<'a> ReadFromPacket<'a> for PacketContentsView<'a> {
 
 impl<'a> WriteToPacket for PacketContentsView<'a> {
     fn max_size_hint(&self) -> usize {
-        todo!()
+        4 + RAND_LEN
+            + 4 // flags
+            + self.from.max_size_hint()
+            + self.from_short.max_size_hint()
+            + self.message.max_size_hint()
+            + self.messages.max_size_hint()
+            + self.address.max_size_hint()
+            + self.seqno.max_size_hint()
+            + self.confirm_seqno.max_size_hint()
+            + self.recv_addr_list_version.max_size_hint()
+            + self.recv_priority_addr_list_version.max_size_hint()
+            + self.reinit_date.max_size_hint()
+            + self.dst_reinit_date.max_size_hint()
+            + self.signature.max_size_hint()
+            + RAND_LEN
     }
 
     fn write_to<T>(&self, packet: &mut T) -> std::io::Result<()>
     where
         T: Write,
     {
-        packet.write(&CONSTRUCTOR.to_le_bytes())?;
-        let mut flag = Flags::empty();
+        // prepare rand buffer
+        let mut rng = rand::thread_rng();
+        let mut rand_bytes = [0; 16];
 
-        flag.set_name(Flags::FROM, &self.from);
-        flag.set_name(Flags::FROM_SHORT, &self.from_short);
-        flag.set_name(Flags::MESSAGE, &self.message);
-        flag.set_name(Flags::MESSAGES, &self.messages);
-        flag.set_name(Flags::ADDRESS, &self.address);
-        flag.set_name(Flags::SEQNO, &self.seqno);
-        flag.set_name(Flags::CONFIRM_SEQNO, &self.confirm_seqno);
-        flag.set_name(Flags::RECV_ADDR_LIST_VERSION, &self.recv_addr_list_version);
-        flag.set_name(
-            Flags::RECV_PRIORITY_ADDR_LIST_VERSION,
+        // prepare flags
+        let mut flags = Flags::default();
+        flags.update(HAS_FROM, &self.from);
+        flags.update(HAS_FROM_SHORT, &self.from_short);
+        flags.update(HAS_MESSAGE, &self.message);
+        flags.update(HAS_MESSAGES, &self.messages);
+        flags.update(HAS_ADDRESS, &self.address);
+        flags.update(HAS_SEQNO, &self.seqno);
+        flags.update(HAS_CONFIRM_SEQNO, &self.confirm_seqno);
+        flags.update(HAS_RECV_ADDR_LIST_VERSION, &self.recv_addr_list_version);
+        flags.update(
+            HAS_RECV_PRIORITY_ADDR_LIST_VERSION,
             &self.recv_priority_addr_list_version,
         );
-        flag.set_name(Flags::REINIT_DATE, &self.reinit_date);
-        flag.set_name(Flags::SIGNATURE, &self.signature);
+        flags.update(HAS_REINIT_DATE, &self.reinit_date);
+        flags.update(HAS_SIGNATURE, &self.signature);
 
-        flag.bits.write_to(packet);
+        // write data
+        ID_PACKET_CONTENTS.write_to(packet)?;
+
+        rng.fill_bytes(&mut rand_bytes);
+        write_bytes(&rand_bytes, packet)?; // rand1
+
+        flags.0.write_to(packet)?;
+
         self.from.write_to(packet)?;
         self.from_short.write_to(packet)?;
         self.message.write_to(packet)?;
-
-        if let Some(a) = &self.messages {
-            a.write_to(packet)?
-        }
+        self.messages.write_to(packet)?;
         self.address.write_to(packet)?;
         self.seqno.write_to(packet)?;
         self.confirm_seqno.write_to(packet)?;
         self.recv_addr_list_version.write_to(packet)?;
+        self.recv_priority_addr_list_version.write_to(packet)?;
         self.reinit_date.write_to(packet)?;
         self.dst_reinit_date.write_to(packet)?;
         self.signature.write_to(packet)?;
-        let mut bytes = [0; 16];
-        rand::thread_rng().fill_bytes(&mut bytes);
-        bytes.write_to(packet)?;
+
+        rng.fill_bytes(&mut rand_bytes);
+        write_bytes(&rand_bytes, packet)?; // rand2
+
         Ok(())
     }
 }
+
+#[derive(Default)]
+struct Flags(u32);
+
+impl Flags {
+    #[inline]
+    fn contains(&self, flag: u32) -> bool {
+        self.0 & flag != 0
+    }
+
+    #[inline]
+    fn update<T>(&mut self, flag: u32, option: &Option<T>) {
+        if option.is_some() {
+            self.0 |= flag;
+        }
+    }
+}
+
+const RAND_LEN: usize = 16;
+
+const HAS_FROM: u32 = 0x0001;
+const HAS_FROM_SHORT: u32 = 0x0002;
+const HAS_MESSAGE: u32 = 0x0004;
+const HAS_MESSAGES: u32 = 0x0008;
+const HAS_ADDRESS: u32 = 0x0010;
+const HAS_PRIORITY_ADDRESS: u32 = 0x0020;
+const HAS_SEQNO: u32 = 0x0040;
+const HAS_CONFIRM_SEQNO: u32 = 0x0080;
+const HAS_RECV_ADDR_LIST_VERSION: u32 = 0x0100;
+const HAS_RECV_PRIORITY_ADDR_LIST_VERSION: u32 = 0x0200;
+const HAS_REINIT_DATE: u32 = 0x0400;
+const HAS_SIGNATURE: u32 = 0x0800;
+
+const ID_PACKET_CONTENTS: u32 = 0xd142cd89;
