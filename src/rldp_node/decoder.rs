e0b52ac96932458638ae3cfb36936cca9e0a64b1
Ivan Kalinin <i.kalinin@dexpa.io>
2022-05-29 02:28:35 +0200

    Fix build

--- src/rldp_node/decoder.rs
+++ src/rldp_node/decoder.rs
@@ -1,7 +1,7 @@
 use ton_api::ton;
 
 pub struct RaptorQDecoder {
-    engine: raptorq::Decoder,
+    engine: everscale_raptorq::Decoder,
     params: ton::fec::type_::RaptorQ,
     seqno: u32,
 }
@@ -9,17 +9,22 @@ pub struct RaptorQDecoder {
 impl RaptorQDecoder {
     pub fn with_params(params: ton::fec::type_::RaptorQ) -> Self {
         Self {
-            engine: raptorq::Decoder::new(raptorq::ObjectTransmissionInformation::with_defaults(
-                params.data_size as u64,
-                params.symbol_size as u16,
-            )),
+            engine: everscale_raptorq::Decoder::new(
+                everscale_raptorq::ObjectTransmissionInformation::with_defaults(
+                    params.data_size as u64,
+                    params.symbol_size as u16,
+                ),
+            ),
             params,
             seqno: 0,
         }
     }
 
     pub fn decode(&mut self, seqno: u32, data: Vec<u8>) -> Option<Vec<u8>> {
-        let packet = raptorq::EncodingPacket::new(raptorq::PayloadId::new(0, seqno), data);
+        let packet = everscale_raptorq::EncodingPacket::new(
+            everscale_raptorq::PayloadId::new(0, seqno),
+            data,
+        );
         self.seqno = seqno;
         self.engine.decode(packet)
     }
--- src/rldp_node/encoder.rs
+++ src/rldp_node/encoder.rs
@@ -2,15 +2,15 @@ use anyhow::Result;
 use ton_api::ton;
 
 pub struct RaptorQEncoder {
-    engine: raptorq::Encoder,
+    engine: everscale_raptorq::Encoder,
     params: ton::fec::type_::RaptorQ,
-    source_packets: Vec<raptorq::EncodingPacket>,
+    source_packets: Vec<everscale_raptorq::EncodingPacket>,
     encoder_index: usize,
 }
 
 impl RaptorQEncoder {
     pub fn with_data(data: &[u8]) -> Self {
-        let engine = raptorq::Encoder::with_defaults(data, MAX_TRANSMISSION_UNIT as u16);
+        let engine = everscale_raptorq::Encoder::with_defaults(data, MAX_TRANSMISSION_UNIT as u16);
         let source_packets = engine
             .get_block_encoders()
             .iter()
