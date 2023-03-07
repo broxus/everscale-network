7d14eb6e6fc7cef223adbecd2f9ff85ca53f4f21
Ivan Kalinin <i.kalinin@dexpa.io>
2023-03-07 21:03:11 +0100

    Simplify dht affinity stuff

--- src/dht/buckets.rs
+++ src/dht/buckets.rs
@@ -46,53 +46,41 @@ impl Buckets {
         T: Borrow<[u8; 32]>,
     {
         let key1 = &self.local_id;
-        let key2 = peer_id.borrow();
+        let key2: &[u8; 32] = peer_id.borrow();
 
-        let mut distance = 0u8;
         let mut nodes = Vec::new();
 
         // Iterate over buckets
         'outer: for i in 0..32 {
-            let mut subdistance = distance;
+            let mut distance = i as u8 * 8;
 
             // Compare bytes
-            let mut xor = key1[i] ^ key2[i];
+            let mut diff = key1[i] ^ key2[i];
 
             // If they are not equal (otherwise we will just add 8 bits
             // to the distance and continue to the next byte)
-            while xor != 0 {
-                if xor & 0xf0 == 0 {
-                    // If high 4 bits of the comparison result are equal then shift comparison
-                    // result to the left, so that low 4 bits will become the high 4 bits
-                    subdistance = subdistance.saturating_add(4);
-                    xor <<= 4;
-                } else {
-                    // Get equal bit count
-                    let shift = BITS[(xor >> 4) as usize];
-                    subdistance = subdistance.saturating_add(shift);
+            while diff != 0 {
+                // Get equal bit count
+                let equal_bits = diff.leading_zeros() as u8; // 0..=7
+                distance += equal_bits;
 
-                    // Add all nodes from this distance to the result
-                    let bucket = &self.buckets[subdistance as usize];
-                    for item in bucket.iter() {
-                        nodes.push(item.value().clone());
-                        if nodes.len() >= k as usize {
-                            break 'outer;
-                        }
+                // Add all nodes from this distance to the result
+                let bucket = &self.buckets[distance as usize];
+                for item in bucket.iter() {
+                    nodes.push(item.value().clone());
+                    if nodes.len() >= k as usize {
+                        break 'outer;
                     }
+                }
 
-                    // Skip one different bit:
-                    // xor = 0000____ | shift + 1 = 5, xor = ________
-                    // xor = 0001____ | shift + 1 = 4, xor = ________
-                    // xor = 001x____ | shift + 1 = 3, xor = x_______
-                    // xor = 01xx____ | shift + 1 = 2, xor = xx______
-                    // xor = 1xxx____ | shift + 1 = 1, xor = xxx_____
-                    xor <<= shift + 1;
-                    subdistance = subdistance.saturating_add(1);
+                // Skip one different bit:
+                if equal_bits < 7 {
+                    diff <<= equal_bits + 1;
+                    distance = distance.saturating_add(1);
+                } else {
+                    continue 'outer;
                 }
             }
-
-            // Increase distance
-            distance = distance.saturating_add(8);
         }
 
         // Done
@@ -111,27 +99,21 @@ impl<'a> IntoIterator for &'a Buckets {
 
 /// Returns the length of the longest common prefix of two keys
 pub fn get_affinity(key1: &[u8; 32], key2: &[u8; 32]) -> u8 {
-    let mut result = 0;
     for i in 0..32 {
-        match key1[i] ^ key2[i] {
-            0 => result += 8,
-            x => {
-                if x & 0xf0 == 0 {
-                    result += BITS[(x & 0x0f) as usize] + 4;
-                } else {
-                    result += BITS[(x >> 4) as usize]
-                }
-                break;
-            }
+        let diff = key1[i] ^ key2[i];
+        if diff != 0 {
+            return (i * 8 + diff.leading_zeros() as usize) as u8;
         }
     }
-    result
+    255
 }
 
-/// XOR  | BITS
-/// 0000 | 4
-/// 0001 | 3
-/// 001x | 2
-/// 01xx | 1
-/// 1xxx | 0
-const BITS: [u8; 16] = [4, 3, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn same_affinity() {
+        assert_eq!(get_affinity(&[0xaa; 32], &[0xaa; 32]), 255);
+    }
+}
